a37dcfc624f3d71cecb027f812fdfd164986006e
HamirA2 <hamiraquino8@gmail.com>
2025-02-18 19:19:05 -0500

    Module 7 completed baseline

---
+++ app_admin/src/app/navbar/navbar.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthenticationService } from '../services/authentication.service';
+import { RouterModule } from '@angular/router';
+
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  constructor(
+    private authenticationService: AuthenticationService
+  ) {}
+
+  ngOnIt() {}
+
+  public isLoggedIn(): boolean {
+    return this.authenticationService.isLoggedIn();
+  }
+  
+  public onLogout(): void {
+    return this.authenticationService.logout();
+  }
+    
+}
